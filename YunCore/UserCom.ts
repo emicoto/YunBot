eb049cbdb4c354d3d1fe7657f2f80d4970489edc
Gaoyao Massimo <ghzero97@gmail.com>
2022-07-20 03:26:32 +0200

    添加灵根

--- YunCore/UserCom.ts
+++ YunCore/UserCom.ts
@@ -2,280 +2,183 @@ import { Context, RuntimeError, segment, Time } from "koishi";
 
 import * as f from "./Function"
 import * as s from "./Setting"
+import { LingGenUtils } from "./utils";
 
-export default function UserCom(ctx: Context){
-	
-	ctx.command('setnick <nick:text>', '设置路昀对你的昵称。优先于用户名。')
-		.alias('callme')
-		.shortcut('叫我')
-		.userFields(['id','nick'])
-		.action( async ({ session }, nick)=>{
-			const { user } = session;
-			if(!nick){
-				if(user.nick){
-					let txt = f.either([
-						`……怎么了，${user.nick}？`,
-						`……有什么事吗？${user.nick}？`,
-						`……好的，${user.nick}。`,
-					])
-					return txt
-				}
-				return '还没有设置昵称'
-			}
+export default function UserCom(ctx: Context) {
 
-			let txt = `${nick}……吗？\n了解了，那么以后就这么称呼${nick}了。\n……还请多多指教。`
-			
-			if(nick.includes("[CQ:")){
-				return '禁止包含纯文本以外的内容'
-			}
-			if(nick.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)){
-				return '不能含有特殊字符'
-			}
-			let check = await ctx.database.get('user',{ nick : nick })
-			if(check.length >= 1 && check[0].onebot!= session.userId){
-				return '不能重名。'
-			}
-			try{
-				user.nick = nick;
-				await user.$update();
-				return txt
-			}
-			catch(error){
-				if(RuntimeError.check(error,"duplicate-entry")){
-					return '昵称跟别人的重复了哦。'
-				}else{
-					ctx.logger("common").warn(error);
-					return "因不明原因设置失败了。详情请看后台日志。"
-				}
-			}	
-		})
-    
-    ctx.command("username <username:text>","设置用户名")
-        .userFields(['id','name'])
-        .action(async ({ session }, username) => {
-            const { user } = session;
-            if( ! username ){
-                if(user.name){
-                    return '你的用户名为：'+user.name
-                }
-                return '你的用户名为空。'
-            }
+  ctx.command('setnick <nick:text>', '设置路昀对你的昵称。优先于用户名。')
+    .alias('callme')
+    .shortcut('叫我')
+    .userFields(['id', 'nick'])
+    .action(async ({ session }, nick) => {
+      const { user } = session;
+      if (!nick) {
+        if (user.nick) {
+          let txt = f.either([
+            `……怎么了，${user.nick}？`,
+            `……有什么事吗？${user.nick}？`,
+            `……好的，${user.nick}。`,
+          ])
+          return txt
+        }
+        return '还没有设置昵称'
+      }
 
-            let txt = `${username}……吗？\n了解了，已经记录到门派档案。`
-			if(username.includes("[CQ:")){
-				return '禁止包含纯文本以外的内容'
-			}
-			if(username.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)){
-				return '不能含有特殊字符'
-			}
-			try{
-				user.name = username;
-				await user.$update();
-				return txt
-			}
-			catch(error){
-				if(RuntimeError.check(error,"duplicate-entry")){
-					return '……不能重名。'
-				}else{
-					ctx.logger("common").warn(error);
-					return "因不明原因设置失败了。详情请看后台日志。"
-				}
-			}
-        })
-    
-    ctx.command("入门申请",'填写入门申请表。')
-        .alias('sign')
-        .action(async ({ session }) => {
-            let uid = session.userId
-            let data = await s.getUser(ctx,uid)
+      let txt = `${nick}……吗？\n了解了，那么以后就这么称呼${nick}了。\n……还请多多指教。`
 
-            if(data.flag?.signed) return '已经注册过了哦。';
-            
-            await session.send('好的，麻烦先在这里填个名字……\n……嗯，要注意的是，不可以有特殊字符或图片、表情哦\n以及用户名与原有的相同可能会导致存档失败。可以用.username查看现有用户名。')
+      if (nick.includes("[CQ:")) {
+        return '禁止包含纯文本以外的内容'
+      }
+      if (nick.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)) {
+        return '不能含有特殊字符'
+      }
+      let check = await ctx.database.get('user', { nick: nick })
+      if (check.length >= 1 && check[0].onebot != session.userId) {
+        return '不能重名。'
+      }
+      try {
+        user.nick = nick;
+        await user.$update();
+        return txt
+      }
+      catch (error) {
+        if (RuntimeError.check(error, "duplicate-entry")) {
+          return '昵称跟别人的重复了哦。'
+        } else {
+          ctx.logger("common").warn(error);
+          return "因不明原因设置失败了。详情请看后台日志。"
+        }
+      }
+    })
 
-            let username = await session.prompt(Time.minute*2)
-            if(!username) return '……写个名字太久了，目前流程已经失效了。'
+  ctx.command("username <username:text>", "设置用户名")
+    .userFields(['id', 'name'])
+    .action(async ({ session }, username) => {
+      const { user } = session;
+      if (!username) {
+        if (user.name) {
+          return '你的用户名为：' + user.name
+        }
+        return '你的用户名为空。'
+      }
 
-            if( username.includes("[CQ:")) return '禁止纯文本意外的内容。'
-            if( username.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)) return '不能含有特殊字符。'
+      let txt = `${username}……吗？\n了解了，已经记录到门派档案。`
+      if (username.includes("[CQ:")) {
+        return '禁止包含纯文本以外的内容'
+      }
+      if (username.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)) {
+        return '不能含有特殊字符'
+      }
+      try {
+        user.name = username;
+        await user.$update();
+        return txt
+      }
+      catch (error) {
+        if (RuntimeError.check(error, "duplicate-entry")) {
+          return '……不能重名。'
+        } else {
+          ctx.logger("common").warn(error);
+          return "因不明原因设置失败了。详情请看后台日志。"
+        }
+      }
+    })
 
-            let check = await ctx.database.get('user',{name:username})
-            if(check.length >= 1 && check[0].onebot != uid) return '不能重名。'
+  ctx.command("入门申请", '填写入门申请表。')
+    .alias('sign')
+    .action(async ({ session }) => {
+      let uid = session.userId
+      let data = await s.getUser(ctx, uid)
 
-            await ctx.database.setUser(session.platform, uid, { name: username })
+      if (data.flag?.signed) return '已经注册过了哦。';
 
-            await session.send(`……名字是【${username}】吗？\n了解了，接下来麻烦填个小问卷，检测下是什么灵根吧。`)
-            await session.send(`不知道${username}一年四季觉得最舒服的是什么时期呢？\n1.春 2.夏 3.秋 4.冬 5.季节变换时`)
+      await session.send('好的，麻烦先在这里填个名字……\n……嗯，要注意的是，不可以有特殊字符或图片、表情哦\n以及用户名与原有的相同可能会导致存档失败。可以用.username查看现有用户名。')
 
-            let answer = await session.prompt(Time.minute*2)
-            if(!answer) return '……填太慢了，目前流程已经失效了……'
+      let username = await session.prompt(Time.minute * 2)
+      if (!username) return '……写个名字太久了，目前流程已经失效了。'
 
-            if(!s.usertoday[uid]) s.initUserToday(uid);
-            s.usertoday[uid]['flag'] = {qa:0, qb:0}
+      if (username.includes("[CQ:")) return '禁止纯文本意外的内容。'
+      if (username.match(/((?=[\x21-\x7e]+)[^A-Za-z0-9])/)) return '不能含有特殊字符。'
 
-            if(answer.match(/1|春/)) s.usertoday[uid]['flag']['qa'] = 1; //木
-            else if(answer.match(/2|夏/)) s.usertoday[uid]['flag']['qa'] = 2; //火
-            else if(answer.match(/3|秋/)) s.usertoday[uid]['flag']['qa'] = 3; //金
-            else if(answer.match(/4|冬/)) s.usertoday[uid]['flag']['qa'] = 4; //水
-            else if(answer.match(/5|季节/)) s.usertoday[uid]['flag']['qa'] = 5; //土
-            else return '……呃，输入无效。请从头来过吧……'
+      let check = await ctx.database.get('user', { name: username })
+      if (check.length >= 1 && check[0].onebot != uid) return '不能重名。'
 
-            await session.send(`……原来如此。那么，东南西北中，你觉得哪个方位让你最舒服？\n1.东 2.南 3.西  4.北  5.中 `)
+      await ctx.database.setUser(session.platform, uid, { name: username })
 
-            answer = await session.prompt(Time.minute*2)
-            if(!answer) return '……填太慢了，目前流程已经失效了……'
+      await session.send(`……名字是【${username}】吗？\n了解了，接下来麻烦填个小问卷，检测下是什么灵根吧。`)
+      await session.send(`不知道${username}一年四季觉得最舒服的是什么时期呢？\n1.春 2.夏 3.秋 4.冬 5.季节变换时`)
 
-            if(answer.match(/1|东/)) s.usertoday[uid]['flag']['qb'] = 1; //木
-            else if(answer.match(/2|南/)) s.usertoday[uid]['flag']['qb'] = 2; //火
-            else if(answer.match(/3|西/)) s.usertoday[uid]['flag']['qb'] = 3; //金
-            else if(answer.match(/4|北/)) s.usertoday[uid]['flag']['qb'] = 4; //水
-            else if(answer.match(/5|中/)) s.usertoday[uid]['flag']['qb'] = 5; //土
-            else return '……呃，输入无效。请从头来过吧……'
+      let answer = await session.prompt(Time.minute * 2)
+      if (!answer) return '……填太慢了，目前流程已经失效了……'
 
-            await session.send(`……了解了。最后，告诉我一个100以内你喜欢的数字。`)
+      if (!s.usertoday[uid]) s.initUserToday(uid);
+      s.usertoday[uid]['flag'] = { qa: 0, qb: 0 }
 
-            answer = await session.prompt(Time.minute*2)
-            if(!answer) return '……填太慢了，目前流程已经失效了……'
+      if (answer.match(/1|春/)) s.usertoday[uid]['flag']['qa'] = 1; //木
+      else if (answer.match(/2|夏/)) s.usertoday[uid]['flag']['qa'] = 2; //火
+      else if (answer.match(/3|秋/)) s.usertoday[uid]['flag']['qa'] = 3; //金
+      else if (answer.match(/4|冬/)) s.usertoday[uid]['flag']['qa'] = 4; //水
+      else if (answer.match(/5|季节/)) s.usertoday[uid]['flag']['qa'] = 5; //土
+      else return '……呃，输入无效。请从头来过吧……'
 
-            let a = answer.match(/\d+/)
-            let d = parseInt(a[0])
+      await session.send(`……原来如此。那么，东南西北中，你觉得哪个方位让你最舒服？\n1.东 2.南 3.西  4.北  5.中 `)
 
-            if(!d) return '……呃，输入无效。请从头来过吧……'
+      answer = await session.prompt(Time.minute * 2)
+      if (!answer) return '……填太慢了，目前流程已经失效了……'
 
-            s.usertoday[uid]['flag']['qc']  = d
+      if (answer.match(/1|东/)) s.usertoday[uid]['flag']['qb'] = 1; //木
+      else if (answer.match(/2|南/)) s.usertoday[uid]['flag']['qb'] = 2; //火
+      else if (answer.match(/3|西/)) s.usertoday[uid]['flag']['qb'] = 3; //金
+      else if (answer.match(/4|北/)) s.usertoday[uid]['flag']['qb'] = 4; //水
+      else if (answer.match(/5|中/)) s.usertoday[uid]['flag']['qb'] = 5; //土
+      else return '……呃，输入无效。请从头来过吧……'
 
-            let result = s.usertoday[uid]['flag']['qa'] + s.usertoday[uid]['flag']['qb'] + s.usertoday[uid]['flag']['qc']
+      await session.send(`……了解了。最后，告诉我一个100以内你喜欢的数字。`)
 
-            result = result%5
+      answer = await session.prompt(Time.minute * 2)
+      if (!answer) return '……填太慢了，目前流程已经失效了……'
 
-            let list = ['木','火','金','水','土']
-            data.flag['signed'] = true
+      let a = answer.match(/\d+/)
+      let d = parseInt(a[0])
 
-            let txt = `……好了，信息已经填好了。${username}的灵根是：`
+      if (!d) return '……呃，输入无效。请从头来过吧……'
 
-            let rate = f.random(100) + ( s.usertoday[uid]['luck'] > 0 ? s.usertoday[uid]['luck']/10 : 0);
-            console.log(username,'灵根测试',rate)
+      s.usertoday[uid]['flag']['qc'] = d
 
-            let qa = s.usertoday[uid]['flag']['qa']-1
-            let qb = s.usertoday[uid]['flag']['qb']-1
+      let result = s.usertoday[uid]['flag']['qa'] + s.usertoday[uid]['flag']['qb'] + s.usertoday[uid]['flag']['qc']
 
-            if(rate >= 90 ){ //单
+      result = result % 5
 
-                data.soul = list[result]
+      data.flag['signed'] = true
 
-                if(f.random(100) > 90){
-                    data.soul = '天'+list[result]
-                }
-        
-            }
+      let txt = `……好了，信息已经填好了。${username}的灵根是：`
 
-            else if(rate >= 80 ){ //双
-                
-                data.soul = list[result]
-                
-                if(result == qb && result != qa ){
+      let rate = f.random(100) + (s.usertoday[uid]['luck'] > 0 ? s.usertoday[uid]['luck'] / 10 : 0);
+      console.log(username, '灵根测试', rate)
 
-                    data.soul += list[qa]
+      const lg = LingGenUtils.getLingGen(rate)
+      const utils = new LingGenUtils(lg, result);
+      data.soul = utils.getSoul();
 
-                }else if(result == qa && result != qb){
+      await s.setUser(ctx, uid, data)
+      return txt + f.printSoul(data.soul)
 
-                    data.soul += list[qb]
+    })
 
-                }
-                else{
-                    
-                    let str = list.join("")
-                    str = str.replace(list[result],"")
-                    str = str[f.random(3)]
-                    data.soul += str
-                    
-                }
+  ctx.command('小灵通', '个人面板')
+    .userFields(['name'])
+    .action(async ({ session }) => {
+      let uid = session.userId
+      let txt = ''
+      let user = await ctx.database.getUser(session.platform, uid)
+      let data = await s.getUser(ctx, uid)
 
-            }
+      let level = f.getLevelChar(data.level)
+      let needexp = f.expLevel(data.level)
+      let soul = f.printSoul(data.soul)
 
-            else if( rate >= 60 ){ //三
-
-                if( result != qb &&  result != qa && qa != qb ){
-                    data.soul = list[result]+list[qb]+list[qa]
-                }
-                else{
-
-                    data.soul = list[result]
-                    let str = '金木水火土'
-
-                    if(result != qb && qb != qa){
-                        data.soul += list[qb]
-                    }
-                    else if(result != qa && qb != qa){
-                        data.soul += list[qa]
-                    }
-
-                    str = str.replace(list[result],"")
-
-                    if(data.soul.length > 1){
-                        str = str.replace(data.soul[1],"")
-                        str = str[f.random(2)]
-                        data.soul += str
-                    }
-                    else{
-                        let a = str[f.random(3)]
-                        data.soul += a
-
-                        let b = str.replace(a,"")
-                        b = b[f.random(2)]
-                        data.soul += b
-                    }                   
-                }
-            }
-
-            else if( rate > 40){ //四
-                let str = '金木水火土'
-                data.soul = list[result]
-                str = str.replace(list[result],"")
-
-                let t=[]
-
-                for(let i=0; i< 4; i++){
-                    let s = str[f.random(str.length-1)]
-                    t[i] = s
-                    str = str.replace(t[i],"")
-                }
-                data.soul += t.join("")
-            }
-
-            else{
-                let str = '金木水火土'
-                let t = []
-                t[0] = list[result]
-                str = str.replace(list[result],"")
-
-                for(let i=1; i<5; i++){
-                    let s = str[f.random(str.length-1)]
-                    t[i] = s
-                    str = str.replace(t[i],"")
-                }
-                data.soul = t.join("")
-            }
-
-            await s.setUser(ctx,uid,data)
-            return txt+f.printSoul(data.soul)
-
-        })
-    
-    ctx.command('小灵通','个人面板')
-        .userFields(['name'])
-        .action( async ({session})=>{
-            let uid = session.userId
-            let txt = ''
-            let user = await ctx.database.getUser(session.platform, uid)
-            let data = await s.getUser(ctx,uid)
-
-            let level = f.getLevelChar(data.level)
-            let needexp = f.expLevel(data.level)
-            let soul = f.printSoul(data.soul)
-
-            txt = `· ${user.name}的个人面板：\n· 昵称：${user.nick}\n${ data.title.length >1 ? `· 头衔：${data.title}\n` : ''}· 灵根：${soul}\n· 境界：${level}\n· 悟道值：${data.exp}/${needexp}\n· 战斗力：${data.BP}\n· 幸运： ${data.luck}\n—————————————————\nHP：${data.HP}/${data.maxHP}  SP:${data.SP}/${data.maxSP}\nAP: ${data.AP}/${data.maxAP}\nATK:${data.ATK} DEF:${data.DEF}\n—————————————————\n· 持有灵石：${data.money}\n`
-            return txt
-        })
+      txt = `· ${user.name}的个人面板：\n· 昵称：${user.nick}\n${data.title.length > 1 ? `· 头衔：${data.title}\n` : ''}· 灵根：${soul}\n· 境界：${level}\n· 悟道值：${data.exp}/${needexp}\n· 战斗力：${data.BP}\n· 幸运： ${data.luck}\n—————————————————\nHP：${data.HP}/${data.maxHP}  SP:${data.SP}/${data.maxSP}\nAP: ${data.AP}/${data.maxAP}\nATK:${data.ATK} DEF:${data.DEF}\n—————————————————\n· 持有灵石：${data.money}\n`
+      return txt
+    })
 
 }
---
+++ YunCore/utils.ts
@@ -0,0 +1,97 @@
+import { random } from "./Function";
+
+
+export enum LingGen {
+  天灵根,
+  双灵根,
+  三灵根,
+  四灵根,
+  杂灵根,
+}
+export enum Elements {
+  火 = "火",
+  土 = "土",
+  木 = "木",
+  水 = "水",
+  金 = "金",
+}
+
+export class LingGenUtils {
+  public static getLingGen(rate: number) {
+    if (rate >= 100) {
+      return LingGen.天灵根
+    }
+    else if (rate >= 80) {
+      return LingGen.双灵根
+    }
+    else if (rate >= 60) {
+      return LingGen.三灵根
+    }
+    else if (rate >= 40) {
+      return LingGen.四灵根
+    }
+    else {
+      return LingGen.杂灵根
+    }
+  }
+  private _LingGen: LingGen;
+  private _Elements: Elements[];
+  private _result: number;
+  constructor(linggen: LingGen, result: number) {
+    this._LingGen = linggen;
+    this._result = result;
+    this.init();
+  }
+  private init() {
+
+    switch (this._LingGen) {
+      case LingGen.天灵根:
+        this._Elements = this.getElements()
+        break;
+      case LingGen.双灵根:
+        this._Elements = this.getElements(2)
+        break;
+      case LingGen.三灵根:
+        this._Elements = this.getElements(3)
+        break;
+      case LingGen.四灵根:
+        this._Elements = this.getElements(4)
+        break;
+      case LingGen.杂灵根:
+        this._Elements = this.getElements(5)
+        break;
+    }
+
+  }
+  private getElements(num: number = 1) {
+    const el = [Elements.金, Elements.木, Elements.水, Elements.火, Elements.土]
+    if (num == 1) {
+      return [el[this._result]]
+    }
+    else if (num >= 5) {
+      return el;
+    }
+    const arr = []
+    arr.push(...el.splice(this._result, 1))
+    const NUM = num - 1;
+    for (let index = 0; index < NUM; index++) {
+      arr.push(...el.splice(random(el.length - 1), 1))
+    }
+    return arr
+  }
+  public getElementsString() {
+    return this._Elements.join("")
+  }
+  public getSoul() {
+    const str = this.getElementsString()
+    switch (this._LingGen) {
+      case LingGen.天灵根:
+        return "天" + str;
+      case LingGen.双灵根:
+      case LingGen.三灵根:
+      case LingGen.四灵根:
+      case LingGen.杂灵根:
+        return str;
+    }
+  }
+}
